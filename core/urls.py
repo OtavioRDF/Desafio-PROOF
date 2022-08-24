05f433e627590ca618d161eb981e18f64e1ef1fd
Otávio Rodrigues <otavio.rodf@gmail.com>
2022-08-24 08:34:49 -0300

    .

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import index, insert, unbanned
 
 urlpatterns = [
     path('list', index, name="list-ips"),
     path('insert', insert, name= 'ban-ips'),
-    path('unbanned', unbanned, name= 'unban-ips')
+    path('list-unbanned', unbanned, name= 'unbanned-ips')
 ]
