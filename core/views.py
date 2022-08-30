5e89a820ce858acb024f8fdbac03b139e6a3b270
Otávio Rodrigues <otavio.rodf@gmail.com>
2022-08-30 02:48:08 -0300

    ajustes front-end

--- core/views.py
+++ core/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
 from .models import Ips, BannedIps
-from .api.serializers import BannedIpSerializer, IpSerializer
 from .webscraping import getIpsTorlist, getIpsOnionoo
 from .forms import BanIpForm
+from django.core.paginator import Paginator
+# from rest_framework.response import Response
+# from rest_framework import status
+# from .api.serializers import BannedIpSerializer, IpSerializer
 
 @api_view(['GET'])
 def index(request):
