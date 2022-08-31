ab721535bca8db6d6fe60cc348a5d91cbd4e68fd
Otávio Rodrigues <otavio.rodf@gmail.com>
2022-08-31 02:49:52 -0300

    ajustes url

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,7 @@ from .views import index, ban_ips, list_all, list_unbanned
 
 urlpatterns = [
     path('', index, name= 'home'),
-    path('ban_ips/', ban_ips, name= 'ban_ips'),
+    path('ban_ips', ban_ips, name= 'ban_ips'),
     path('list', list_all, name= 'list_ips'),
-    #path('ban-ips', insert, name= 'ban_ips'),
-    path('list-unbanned', list_unbanned, name= 'list_unbanned')
+    path('list_unbanned', list_unbanned, name= 'list_unbanned')
 ]
